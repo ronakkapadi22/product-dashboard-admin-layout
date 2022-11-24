40a9e506abd58e67b7ec50d4d25ef072f390bfd8
Ronak Kapadi <ronakk@itpathsolutions.com>
2022-11-24 07:39:05 +0530

    Signup page completed.

--- src/Router/Routing.js
+++ src/Router/Routing.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import AuthLayout from '../presentation/layout/AuthLayout'
+import PublicLayout from '../presentation/layout/PublicLayout'
+import NotFound from '../presentation/NotFound'
+import { authRoutes, publicRoutes } from './routes'
 
-const Routing = ({...props}) => {
+const Routing = ({ isLoggedIn, ...props }) => {
   return (
-    <div {...props} >Routing</div>
+    <Routes {...props} >
+      <Route path='/' element={<PublicLayout isDefaultAccess />} >
+        {publicRoutes(true).map(({ id, ...otherProps }) => <Route index key={id} {...otherProps} />)}
+      </Route>
+      <Route path='/' element={<AuthLayout isLoggedIn={isLoggedIn} />} >
+        {authRoutes(true).map(({ id, ...otherProps }) => <Route index key={id} {...otherProps} />)}
+      </Route>
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
